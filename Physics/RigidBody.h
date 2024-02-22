790455b73e4e9d84703fc7b7d0de8543ea34adcd
Jay Pounsett <pounsett.jay@gmail.com>
2024-02-22 21:52:18 +1030

    Reformatted

--- Physics/RigidBody.h
+++ Physics/RigidBody.h
@@ -2,28 +2,26 @@
 #include "PhysicsObject.h"
 #include <glm/glm.hpp>
 
-class RigidBody : public PhysicsObject
-{
+class RigidBody : public PhysicsObject {
 public:
-	RigidBody(ShapeType shapeID, glm::vec2 position,
-		glm::vec2 velocity, float orientation,
-		float mass);
-	~RigidBody();
+  RigidBody(ShapeType shapeID, glm::vec2 position, glm::vec2 velocity,
+			float orientation, float mass);
+  ~RigidBody();
 
-	void fixedUpdate(glm::vec2 gravity, float timeStep) override;
-	void applyForce(glm::vec2 force);
-	void applyForceToActor(RigidBody* actor2, glm::vec2 force);
+  void fixedUpdate(glm::vec2 gravity, float timeStep) override;
+  void applyForce(glm::vec2 force);
+  void applyForceToActor(RigidBody *actor2, glm::vec2 force);
 
-	glm::vec2 getPosition() { return m_position; }
-	float getOrientation() { return m_orientation; }
-	glm::vec2 getVelocity() { return m_velocity; }
-	float getMass() { return m_mass; }
-	void setMass(float m) { m_mass = m; }
+  glm::vec2 getPosition() { return m_position; }
+  float getOrientation() { return m_orientation; }
+  glm::vec2 getVelocity() { return m_velocity; }
+  float getMass() { return m_mass; }
+  void setMass(float m) { m_mass = m; }
 
 protected:
-	glm::vec2 m_position;
-	glm::vec2 m_velocity;
-	float m_mass;
-	float m_orientation; //2D so we only need a single float to represent orientation
+  glm::vec2 m_position;
+  glm::vec2 m_velocity;
+  float m_mass;
+  float m_orientation; // 2D so we only need a single float to represent
+					   // orientation
 };
-
--- Physics/Sphere.h
+++ Physics/Sphere.h
@@ -1,20 +1,18 @@
 #pragma once
 #include "RigidBody.h"
 
-class Sphere : public RigidBody
-{
+class Sphere : public RigidBody {
 public:
-	Sphere(glm::vec2 position, glm::vec2 velocity, 
-		float mass, float radius, glm::vec4 colour);
-	~Sphere();
+  Sphere(glm::vec2 position, glm::vec2 velocity, float mass, float radius,
+		 glm::vec4 colour);
+  ~Sphere();
 
-	virtual void draw();
+  virtual void draw();
 
-	float getRadius() { return m_radius; }
-	glm::vec4 getColour() { return m_colour; }
+  float getRadius() { return m_radius; }
+  glm::vec4 getColour() { return m_colour; }
 
 protected:
-	float m_radius;
-	glm::vec4 m_colour;
+  float m_radius;
+  glm::vec4 m_colour;
 };
-
